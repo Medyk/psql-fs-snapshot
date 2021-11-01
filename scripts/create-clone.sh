2d9cad09a13ad3ee7d47e549dff87f5929d93c5a
Maciej Medyński <maciej@medynski.waw.pl>
2021-11-01 21:13:32 +0100

    Bug fixes.

--- scripts/create-clone.sh
+++ scripts/create-clone.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export $(grep -v '^#' ../docker/.env | xargs)
 PORT="$1"
 
 # FIXME: start backup and stop backup for snapshot
@@ -13,13 +14,13 @@ btrfs subvolume snapshot /btrfs/psql_master "/btrfs/psql_clone_${PORT}"
 docker-compose --project-directory ../docker start psql_master
 
 # Create new clone
-export $(grep -v '^#' ../docker/.env | xargs)
 docker run \
-    --name "psql_clone_${PORT}" \
+    --name "${COMPOSE_PROJECT_NAME}_psql_clone_${PORT}" \
+    --detach \
     -p ${PORT}:5432 \
     -v ${BTRFS_ROOT:-/btrfs}/psql_clone_${PORT}/data:/var/lib/postgresql/data \
     -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-postgres} \
     --restart=always \
     --shm-size=64M \
-    --network=psql-fs-snapshot_default \
+    --network=${COMPOSE_PROJECT_NAME}_default \
     postgres:${DOCKER_IMAGE_POSTGRES_TAG:-latest}
