57a2cbee4cf8131fc7e657ec29c9ef67fda751f4
Maciej Medyński <maciej@medynski.waw.pl>
2021-11-01 21:37:38 +0100

    Added second table.

--- docker/psql_master-initdb.d/50-init-random-data.sql
+++ docker/psql_master-initdb.d/50-init-random-data.sql
@@ -5,5 +5,13 @@ CREATE TABLE IF NOT EXISTS "random_data" (
     "value" uuid NOT NULL DEFAULT gen_random_uuid()
 );
 
+CREATE TABLE IF NOT EXISTS "random_data2" (
+    "id" bigserial PRIMARY KEY,
+    "value" uuid NOT NULL DEFAULT gen_random_uuid()
+);
+
 INSERT INTO "random_data" ("value")
+    SELECT gen_random_uuid() FROM generate_series(1, 1000000);
+
+INSERT INTO "random_data2" ("value")
     SELECT gen_random_uuid() FROM generate_series(1, 5000000);
