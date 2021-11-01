9b6b8fdb3b3321b734bcc574e3f858b45307ba49
Maciej Medyński <maciej@medynski.waw.pl>
2021-11-01 21:40:01 +0100

    Fixed destroy clone script.

--- scripts/destroy-clone.sh
+++ scripts/destroy-clone.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+export $(grep -v '^#' ../docker/.env | xargs)
 PORT="$1"
 
 # Destroy clone
-docker rm -f "psql_clone_${PORT}"
+docker rm -f "${COMPOSE_PROJECT_NAME}_psql_clone_${PORT}"
 
 # Destroy snapshot
 btrfs subvolume delete "/btrfs/psql_clone_${PORT}"
